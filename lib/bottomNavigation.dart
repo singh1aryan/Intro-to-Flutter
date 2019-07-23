9fda91d93fc16d61a954a8c2b8e711f98f28db29
Aryan Singh <Aryan.Singh@arrow.com>
2019-07-23 15:37:08 -0600

    bottom nav added

---
+++ lib/bottomNavigation.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+import 'home.dart';
+
+class App extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return MaterialApp(
+      home: BottomNavigation(),
+    );
+  }
+
+}
+
+class BottomNavigation extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => new _BottomNavigationState();
+}
+
+class _BottomNavigationState extends State<BottomNavigation>{
+  int _currentIndex = 0;
+  final List<Widget> _children = [
+    Home(),
+    Home(),
+    Home()
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: _children[_currentIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        onTap: onTabTapped,
+        currentIndex: _currentIndex, // this will be set when a new tab is tapped
+        items: [
+          BottomNavigationBarItem(
+            icon: new Icon(Icons.video_label),
+            title: new Text('Arrow'),
+          ),
+          BottomNavigationBarItem(
+            icon: new Icon(Icons.camera),
+            title: new Text('News'),
+          ),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.insert_chart),
+              title: Text('Sales')
+          )
+        ],
+      ),
+    );
+  }
+
+  void onTabTapped(int index){
+    setState(() {
+      _currentIndex = index;
+    });
+  }
+}
