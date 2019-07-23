214e1cc174549fd06913fafc42bc9107777ed3b2
Aryan Singh <Aryan.Singh@arrow.com>
2019-07-23 11:59:16 -0600

    first commit

---
+++ android/app/src/main/java/aryan/easy_recipe/MainActivity.java
@@ -0,0 +1,13 @@
+package aryan.easy_recipe;
+
+import android.os.Bundle;
+import io.flutter.app.FlutterActivity;
+import io.flutter.plugins.GeneratedPluginRegistrant;
+
+public class MainActivity extends FlutterActivity {
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    GeneratedPluginRegistrant.registerWith(this);
+  }
+}
