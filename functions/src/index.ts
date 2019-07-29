1dc702a5aa73c60ee60c168485027f196a7d9fff
Aryan Singh <Aryan.Singh@arrow.com>
2019-07-29 11:03:05 -0600

    notification added wohoooo, with sound

---
+++ functions/src/index.ts
@@ -0,0 +1,29 @@
+import * as functions from 'firebase-functions';
+import * as admin from 'firebase-admin';
+admin.initializeApp()
+
+// const db = admin.firestore();
+const fcm = admin.messaging();
+
+export const sendToTopic = functions.firestore
+    .document('recipes/{recipyId}')
+    .onCreate(async snapshot => {
+        // const recipe = snapshot.data();
+        const payload : admin.messaging.MessagingPayload = {
+            notification: {
+                title: 'new recipe',
+                body: 'new body'
+            }
+        };
+
+        return fcm.sendToTopic('recipes', payload);
+
+    });
+
+
+// // Start writing Firebase Functions
+// // https://firebase.google.com/docs/functions/typescript
+//
+// export const helloWorld = functions.https.onRequest((request, response) => {
+//  response.send("Hello from Firebase!");
+// });
