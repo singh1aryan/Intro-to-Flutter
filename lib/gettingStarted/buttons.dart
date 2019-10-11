8181c55f21a037e8285a22e9a5b92165baa095f4
aryan singh <aryansingh@umass.edu>
2019-10-11 15:56:20 -0400

    introduction

---
+++ lib/gettingStarted/buttons.dart
@@ -0,0 +1,21 @@
+class App extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return MaterialApp(
+      home: Home(),
+      theme: ThemeData(primaryColor: PrimaryColor),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
+
+class Home extends StatefulWidget{
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return new _HomeState();
+  }
+}
+
+class _HomeState extends State<Home>{}
